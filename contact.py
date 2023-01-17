8d234427c5f73ffb11cbd4c74ffee1b94d2d484a
momsstancer <pavel.str94@gmail.com>
2023-01-17 23:07:23 +0300

    -

---
+++ contact.py
@@ -0,0 +1,24 @@
+class Info:
+
+    def __init__(self, name1, name2, name3, nickname, title, companyname, adress):
+        self.name1 = name1
+        self.name2 = name2
+        self.name3 = name3
+        self.nickname = nickname
+        self.title = title
+        self.companyname = companyname
+        self.adress = adress
+
+class Tele:
+
+    def __init__(self, telefon1, telefon2, telefon3, fax, box1, box2, box3, url):
+        self.telefon1 = telefon1
+        self.telefon2 = telefon2
+        self.telefon3 = telefon3
+        self.fax = fax
+        self.box1 = box1
+        self.box2 = box2
+        self.box3 = box3
+        self.url = url
+
+
