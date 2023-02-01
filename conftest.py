0f39f5c8195f50a479f2ff2b347b2e68b7cc5944
momsstancer <pavel.str94@gmail.com>
2023-02-02 02:04:41 +0300

    Scope=Session

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ from model.group import Group
 import pytest
 from fixture.application import Application
 
-@pytest.fixture
+@pytest.fixture (scope="session")
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
